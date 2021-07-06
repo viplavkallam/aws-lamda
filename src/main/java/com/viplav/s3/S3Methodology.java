92ba37bd894cb5578a7bb136fcb40db84181b58f
viplav-kallam <75678708+viplav-kallam@users.noreply.github.com>
2021-07-06 18:46:18 +0530

    first commit

---
+++ src/main/java/com/viplav/s3/S3Methodology.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.viplav.s3;
+
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+/**
+ * @author vkallam2
+ *
+ */
+public class S3Methodology {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	    public String handleRequest(Map<String,String> input, Context context) {
+	        context.getLogger().log("Hello:::::: " + input.get("name"));
+	        return "Hello World - " + input;
+	}
+}
